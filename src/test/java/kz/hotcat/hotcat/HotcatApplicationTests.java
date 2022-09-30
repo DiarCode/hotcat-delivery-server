f38520481e8f5789994ee527ce92c015937254a7
DiarCode <diar_beg64@mail.ru>
2022-09-30 13:12:12 +0600

    initialize app

---
+++ src/test/java/kz/hotcat/hotcat/HotcatApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.hotcat.hotcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotcatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
