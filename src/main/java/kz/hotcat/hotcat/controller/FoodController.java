bf9e185658f38d81f33a41388bd28f5b18ff3a33
DiarCode <diar_beg64@mail.ru>
2022-10-03 22:38:32 +0600

    basic food logic implemented

--- src/main/java/kz/hotcat/hotcat/controller/FoodController.java
+++ src/main/java/kz/hotcat/hotcat/controller/FoodController.java
@@ -1,13 +1,36 @@
 package kz.hotcat.hotcat.controller;
 
+import kz.hotcat.hotcat.entity.DeliveryProvider;
+import kz.hotcat.hotcat.entity.Food;
 import kz.hotcat.hotcat.service.FoodService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/foods")
 @AllArgsConstructor
 public class FoodController {
     private final FoodService foodService;
+
+    @GetMapping
+    public List<Food> getAllFoods() {
+        return foodService.getAllFoods();
+    }
+
+    @GetMapping("/{id}")
+    public Food getFoodById(@PathVariable(name = "id") Long foodId) {
+        return foodService.getFoodById(foodId);
+    }
+
+    @PostMapping
+    public Food createNewFood(@RequestBody Food food) {
+        return foodService.createNewFood(food);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFoodById(@PathVariable(name = "id") Long foodId) {
+        foodService.deleteFoodById(foodId);
+    }
 }
--- src/main/java/kz/hotcat/hotcat/service/FoodService.java
+++ src/main/java/kz/hotcat/hotcat/service/FoodService.java
@@ -1,11 +1,37 @@
 package kz.hotcat.hotcat.service;
 
+import kz.hotcat.hotcat.entity.Food;
 import kz.hotcat.hotcat.repository.FoodRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class FoodService {
     private final FoodRepository foodRepository;
+
+    public List<Food> getAllFoods() {
+        return  foodRepository.findAll();
+    }
+
+    public Food getFoodById(Long foodId) {
+        return foodRepository.findById(foodId)
+                .orElseThrow(() -> new RuntimeException("No such food"));
+    }
+
+    public Food createNewFood(Food food) {
+        return foodRepository.save(food);
+    }
+
+    public void deleteFoodById(Long foodId) {
+        boolean isFoodExists = foodRepository.existsById(foodId);
+
+        if(!isFoodExists) {
+            throw new RuntimeException("No such food");
+        }
+
+        foodRepository.deleteById(foodId);
+    }
 }
